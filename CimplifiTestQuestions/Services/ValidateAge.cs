e58ff87f7cb21185001f49ab1f85371dcba99e85
gstill498463 <gabriel.m.still@gmail.com>
2024-09-10 14:55:25 -0700

    created custom agevalidation exception, validator and testing method

---
+++ CimplifiTestQuestions/Services/ValidateAge.cs
@@ -0,0 +1,21 @@
+﻿using CimplifiTestQuestions.CustomExceptions;
+
+namespace CimplifiTestQuestions.Services
+{
+    public class ValidateAge
+    {
+        //this method validates the age and uses the custom InvalidAgeException exception.
+        public void ValidateGivenAge(int age)
+        {
+            if (age < 0 || age > 120)
+            {
+                throw new InvalidAgeException("This age is invalid, please enter a valid age.");
+            }
+            else
+            {
+                Console.WriteLine("Valid age!");
+            }
+
+        }
+    }
+}
