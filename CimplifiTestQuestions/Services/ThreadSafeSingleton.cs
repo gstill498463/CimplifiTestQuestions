b35e519d8ffa57d13280b56690e68e2c6f1d1cee
gstill498463 <gabriel.m.still@gmail.com>
2024-09-10 13:49:25 -0700

    created a thread safe singleton

---
+++ CimplifiTestQuestions/Services/ThreadSafeSingleton.cs
@@ -0,0 +1,25 @@
+﻿namespace CimplifiTestQuestions.Services
+{
+    public class ThreadSafeSingleton
+    {
+        //thread safe using lazy initialization, making sure the instance is created only when needed
+        private static readonly Lazy<ThreadSafeSingleton> _instance = new Lazy<ThreadSafeSingleton>(() => new ThreadSafeSingleton());
+
+        //private constructor prevents immediate initialization
+        private ThreadSafeSingleton()
+        {
+        }
+
+        //gets instance of singleton class with a public method
+        public static ThreadSafeSingleton GetInstance()
+        {
+            return _instance.Value;
+        }
+
+
+        public string RewardingSingletonCode()
+        {
+            return "nicely done!";
+        }
+    }
+}
